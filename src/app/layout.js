ce4028048e3781e5340b14696bd4aa2f4d89e6b1
MSTY-Enterprises <179815051+MSTY-Enterprises@users.noreply.github.com>
2025-07-22 17:27:12 +0530

    google ads tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,17 +14,24 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 const inter = Inter_Tight({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
-
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17000850069"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17000850069');
+          `}
+        </script>
+      </head>
       <body className={`${inter.className} antialiased relative`}>
         <WhatsAppbutton />
         <BlogProvider>
